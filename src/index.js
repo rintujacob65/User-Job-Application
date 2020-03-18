44f0b890a69ab896ab60cae68ae7f191c26696bd
rintujacob65 <rintujacob65@gmail.com>
2020-03-18 19:38:39 +0400

    User Job Application

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import configureStore from './store/configureStore'
+import { Provider } from 'react-redux'
+import {startSetUsers, startUpdateStatus,startSendUser} from './actions/userAction'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+console.log(store.getState())
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.subscribe(() => {
+    console.log(store.getState())
+})
+store.dispatch(startSetUsers())
+if(store.users != null){
+    store.dispatch(startSendUser())
+store.dispatch(startUpdateStatus())
+}
+
+const jsx = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+ReactDOM.render(jsx,document.getElementById('root'))
